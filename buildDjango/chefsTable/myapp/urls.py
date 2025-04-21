7e4617417c8feb742c36918c831c4f138b364f9b
tharindug99 <tharindunirmala99@gmail.com>
2025-04-21 06:27:56 +0530

    Add new views and update URLs; implement form handling in templates

--- buildDjango/chefsTable/myapp/urls.py
+++ buildDjango/chefsTable/myapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views  # Import views from the current directory
 
+
 urlpatterns = [
     path('',views.home),  # Home page
     path('say_hello/',views.say_hello),  # Hello page
     path('about/',views.about),  # About page
+    path('getuser/<name>/<id>', views.pathview, name='pathview'), # Query Params
+    path('getuser/', views.qryview, name='qryview'), 
+    path('showform/', views.showform, name='showform'),
+    path('getform/', views.getform, name='getform'),
 ]
--- buildDjango/chefsTable/myapp/views.py
+++ buildDjango/chefsTable/myapp/views.py
@@ -20,3 +20,20 @@ def say_hello(request):
 
 def about(request):
     return HttpResponse("This is the about page.")
+
+def pathview(request, name, id): 
+    return HttpResponse("Name:{} <br> UserID:{}".format(name, id))
+
+def qryview(request): 
+    name = request.GET['name'] 
+    id = request.GET['id'] 
+    return HttpResponse("Name:{} UserID:{}".format(name, id))
+
+def showform(request):
+     return render(request, "form.html")
+
+def getform(request): 
+    if request.method == "POST": 
+        id=request.POST['id'] 
+        name=request.POST['name'] 
+    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
