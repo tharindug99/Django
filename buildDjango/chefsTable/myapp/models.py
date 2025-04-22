0e38645441f0fe8ae85f9cf184efa942633ffc89
tharindug99 <tharindunirmala99@gmail.com>
2025-04-22 06:24:35 +0530

    Add Person model with fields and migrations; update settings and views

--- buildDjango/chefsTable/myapp/models.py
+++ buildDjango/chefsTable/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model): 
+    Person_name = models.CharField(max_length=20) 
+    age = models.IntegerField(default=0)
+    email = models.EmailField() 
+    phone = models.CharField(max_length=20) 
--- buildDjango/chefsTable/myapp/views.py
+++ buildDjango/chefsTable/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 # Create your views here.
-from django.http import HttpResponse
+
 
 def home(request):
     return HttpResponse("Hello, Django!")
